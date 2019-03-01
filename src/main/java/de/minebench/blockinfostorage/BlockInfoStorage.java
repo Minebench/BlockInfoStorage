b25ff55b00abe54657abe95b170cf7aa424cb06c
Phoenix616 <mail@moep.tv>
2019-03-02 00:50:10 +0100

    Initial commit

---
+++ src/main/java/de/minebench/blockinfostorage/BlockInfoStorage.java
@@ -0,0 +1,273 @@
+package de.minebench.blockinfostorage;
+
+/*
+ * BlockInfoStorage
+ * Copyright (c) 2019 Max Lee aka Phoenix616 (mail@moep.tv)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.Chunk;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.event.world.WorldSaveEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+
+public class BlockInfoStorage extends JavaPlugin implements Listener {
+
+    private static BlockInfoStorage instance;
+
+    private final Map<UUID, Map<Integer, Map<Integer, Region.Location>>> regionLocs = new HashMap<>();
+    private final Map<Region.Location, Region> dataMap = new ConcurrentHashMap<>();
+
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    // --- "API" ---
+
+    /**
+     * Get the storage instance
+     * @return The storage instance
+     */
+    public static BlockInfoStorage get() {
+        return instance;
+    }
+
+    /**
+     * Set block info
+     * @param block The block to attach the info to
+     * @param key   The key to set
+     * @param value The value to set
+     */
+    public void setBlockInfo(Block block, NamespacedKey key, Object value) {
+        getRegion(block).setInfo(block.getX(), block.getY(), block.getZ(), key, value);
+    }
+
+    /**
+     * Get information from a block. This is only for reading, use {@link #setBlockInfo(Block, NamespacedKey, Object)} for writing!
+     * @param block     The block to attach the info to
+     * @param plugin    The plugin for the info
+     * @return All the info that the plugin assigned to the block or null if none was found
+     */
+    public ConfigurationSection getBlockInfo(Block block, Plugin plugin) {
+        return getBlockInfo(block, plugin.getName());
+    }
+
+    /**
+     * Get information from a block. This is only for reading, use {@link #setBlockInfo(Block, NamespacedKey, Object)} for writing!
+     * @param block The block to attach the info to
+     * @param type  The type of info to get
+     * @return All the info of that type that is assigned to the block or null if none was found
+     */
+    public ConfigurationSection getBlockInfo(Block block, String type) {
+        return getRegion(block).getInfo(block.getX(), block.getY(), block.getZ(), type.toLowerCase(Locale.ENGLISH));
+    }
+
+    /**
+     * Get information from a block. This is only for reading, use {@link #setBlockInfo(Block, NamespacedKey, Object)} for writing!
+     * @param block The block to attach the info to
+     * @param key   The namespaced key for the info to get
+     * @return The info value assigned to the block or null if none was found
+     */
+    public Object getBlockInfoValue(Block block, NamespacedKey key) {
+        return getRegion(block).getInfoValue(block.getX(), block.getY(), block.getZ(), key);
+    }
+
+    /**
+     * Remove block info
+     * @param block     The block the info is attached to
+     * @param plugin    The plugin which's info to remove
+     */
+    public void removeBlockInfo(Block block, Plugin plugin) {
+        removeBlockInfo(block, plugin.getName());
+    }
+
+    /**
+     * Remove block info
+     * @param block The block the info is attached to
+     * @param key   The key to remove
+     */
+    public void removeBlockInfo(Block block, NamespacedKey key) {
+        getRegion(block).removeInfo(block.getX(), block.getZ(), block.getY(), key.getNamespace() + "." + key.getKey());
+    }
+
+    /**
+     * Remove block info
+     * @param block The block the info is attached to
+     * @param type  The type to remove
+     */
+    public void removeBlockInfo(Block block, String type) {
+        getRegion(block).removeInfo(block.getX(), block.getZ(), block.getY(), type.toLowerCase(Locale.ENGLISH));
+    }
+
+    /**
+     * Remove all block info (TODO: Decide whether or not this is too dangerous to expose)
+     * @param block The block info was attached to
+     */
+    public void removeBlockInfo(Block block) {
+        getRegion(block).removeInfo(block.getX(), block.getY(), block.getZ());
+    }
+
+    // --- Events ---
+
+    /**
+     * Cache chunk data
+     */
+    @EventHandler(priority = EventPriority.MONITOR)
+    private void onChunkLoad(ChunkLoadEvent event) {
+        Region.Location location = getRegionLocation(event.getChunk());
+        synchronized (location) {
+            if (!dataMap.containsKey(location)) {
+                getServer().getScheduler().runTaskAsynchronously(this, () -> {
+                    synchronized (location) {
+                        load(location).notifyLoad(event.getChunk().getX(), event.getChunk().getZ());
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * Remove cached chunk data
+     */
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void onChunkUnload(ChunkUnloadEvent event) {
+        Region.Location regionLocation = getRegionLocation(event.getChunk());
+        synchronized (regionLocation) {
+            Region region = dataMap.get(regionLocation);
+            if (region != null) {
+                region.notifyUnload(event.getChunk().getX(), event.getChunk().getZ());
+                if (region.isUnloaded()) {
+                    dataMap.remove(regionLocation);
+                    save(region);
+                }
+            }
+        }
+    }
+
+    /**
+     * Save chunk data to disk to ensure being in-sync with the world
+     */
+    @EventHandler(priority = EventPriority.MONITOR)
+    private void onWorldSave(WorldSaveEvent event) {
+        for (Region region : dataMap.values()) {
+            save(region);
+        }
+    }
+
+    // --- Block update handling ---
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void on(BlockBreakEvent event) {
+        removeBlockInfo(event.getBlock());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void on(BlockPlaceEvent event) {
+        removeBlockInfo(event.getBlock());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void on(BlockBurnEvent event) {
+        removeBlockInfo(event.getBlock());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void on(BlockFadeEvent event) {
+        removeBlockInfo(event.getBlock());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void on(BlockPistonExtendEvent event) {
+        // TODO: Block info moving?
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void on(BlockPistonRetractEvent event) {
+        // TODO: Block info moving?
+    }
+
+    // --- Internal utility methods ---
+
+    private Region load(Region.Location location) {
+        Region region = dataMap.get(location);
+        if (region != null) {
+            return region;
+        }
+        region = new Region(location);
+
+        // load data from file
+        region.load();
+
+        dataMap.put(location, region);
+        return region;
+    }
+
+    private void save(Region region) {
+        try {
+            region.save();
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Error while trying to save block info for region " + region.getLocation(), e);
+        }
+    }
+
+    private Region getRegion(Block block) {
+        Region.Location location = getRegionLocation(block);
+        synchronized (location) {
+            return load(location);
+        }
+    }
+
+    private Region.Location getRegionLocation(World world, int x, int z) {
+        synchronized (regionLocs) {
+            return regionLocs.computeIfAbsent(world.getUID(), k -> new HashMap<>())
+                    .computeIfAbsent(x, k -> new HashMap<>())
+                    .computeIfAbsent(z, k -> new Region.Location(world.getUID(), x, z));
+        }
+    }
+
+    private Region.Location getRegionLocation(Chunk chunk) {
+        return getRegionLocation(chunk.getWorld(), chunk.getX() >> 5, chunk.getZ() >> 5);
+    }
+
+    private Region.Location getRegionLocation(Block block) {
+        return getRegionLocation(block.getWorld(), block.getX() >> 9, block.getZ() >> 9);
+    }
+}
---
+++ src/main/java/de/minebench/blockinfostorage/Region.java
@@ -0,0 +1,173 @@
+package de.minebench.blockinfostorage;
+
+/*
+ * BlockInfoStorage
+ * Copyright (c) 2019 Max Lee aka Phoenix616 (mail@moep.tv)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+class Region {
+
+    private final Location location;
+    private Map<Integer, Integer> loaded = new HashMap<>();
+
+    private boolean modified = false;
+
+    private FileConfiguration config = null;
+
+    Region(Location location) {
+        this.location = location;
+    }
+
+    /**
+     * Notify this holder that a certain chunk got loaded
+     * @param x The X coordinate of the chunk
+     * @param z The Z coordinate of the chunk
+     */
+    void notifyLoad(int x, int z) {
+        loaded.put(x, z);
+    }
+
+    /**
+     * Notify this holder that a certain chunk got unloaded
+     * @param x The X coordinate of the chunk
+     * @param z The Z coordinate of the chunk
+     */
+    void notifyUnload(int x, int z) {
+        loaded.remove(x, z);
+    }
+
+    /**
+     * Check whether or not a region is fully unloaded
+     * @return true if no chunk is loaded anymore, false if there stilla re some
+     */
+    boolean isUnloaded() {
+        return loaded.isEmpty();
+    }
+
+    /**
+     * Load the data from the disk
+     */
+    void load() {
+        if (config != null) {
+            throw new IllegalStateException("Config is already loaded!");
+        }
+        File configFile = getConfigFile();
+        if (configFile.exists()) {
+            config = YamlConfiguration.loadConfiguration(getConfigFile());
+        } else {
+            config = new YamlConfiguration();
+        }
+    }
+
+    private File getConfigFile() {
+        World world = Bukkit.getWorld(location.worldId);
+        if (world == null) {
+            throw new IllegalStateException("Could not find world with ID " + location.worldId + "?");
+        }
+        return new File(new File(world.getWorldFolder(), "blockinfo"), "bi." + location.x + "." + location.z + ".yml");
+    }
+
+    synchronized void save() throws IOException {
+        if (!modified) {
+            return;
+        }
+
+        File configFile = getConfigFile();
+        configFile.mkdirs();
+        config.save(configFile);
+        modified = false;
+    }
+
+    private void ensureLoaded() {
+        if (config == null) {
+            throw new IllegalStateException("Config isn't loaded yet!");
+        }
+    }
+
+    void setInfo(int x, int y, int z, NamespacedKey key, Object value) {
+        ensureLoaded();
+        config.set(x + "." + y + "." + z + "." + key.getNamespace() + "." + key.getKey(), value);
+        modified = true;
+    }
+
+    Object getInfoValue(int x, int y, int z, NamespacedKey key) {
+        ensureLoaded();
+        return config.get(x + "." + y + "." + z + "." + key.getNamespace() + "." + key.getKey());
+    }
+
+    ConfigurationSection getInfo(int x, int y, int z, String key) {
+        ensureLoaded();
+        return config.getConfigurationSection(x + "." + y + "." + z + "." + key);
+    }
+
+    void removeInfo(int x, int y, int z) {
+        ensureLoaded();
+        config.set(x + "." + y + "." + z, null);
+        if (config.getConfigurationSection(x + "." + y).getKeys(false).isEmpty()) {
+            config.set(x + "." + y, null);
+            if (config.getConfigurationSection(String.valueOf(x)).getKeys(false).isEmpty()) {
+                config.set(String.valueOf(x), null);
+            }
+        }
+        modified = true;
+    }
+
+    void removeInfo(int x, int z, int y, String key) {
+        ensureLoaded();
+        config.set(x + "." + y + "." + z + "." + key, null);
+        if (config.getConfigurationSection(x + "." + y + "." + z).getKeys(false).isEmpty()) {
+            removeInfo(x, y, z);
+        }
+        modified = true;
+    }
+
+    Location getLocation() {
+        return location;
+    }
+
+    static class Location {
+        private final UUID worldId;
+        private final int x;
+        private final int z;
+
+        Location(UUID worldId, int x, int z) {
+            this.worldId = worldId;
+            this.x = x;
+            this.z = z;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = worldId.hashCode();
+            result = 31 * result + x;
+            result = 31 * result + z;
+            return result;
+        }
+    }
+}
